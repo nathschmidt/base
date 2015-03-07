0663ca831bcef6be96f0b561f197b05bacff38d0
James Mills <prologic@shortcircuit.net.au>
2015-03-07 18:59:57 +1000

    Add finit to replace busybox init. (Closes #5)

--- test.sh
+++ test.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+gui=0
+
+while getopts g opt; do
+  case $opt in
+  g)
+      gui=1
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
 echo "Booting..."
 echo
 
-qemu-system-x86_64 -nographic -cdrom image.iso -net bridge,br=br0 -net nic
+if (( gui == 1 )); then
+  qemu-system-x86_64 -cdrom image.iso -net bridge,br=br0 -net nic
+else
+  qemu-system-x86_64 -nographic -cdrom image.iso -net bridge,br=br0 -net nic
+fi
