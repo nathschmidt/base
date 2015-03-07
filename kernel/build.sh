02b03850278ad7399e687fc8c2eb17c83bbe0c97
James Mills <prologic@shortcircuit.net.au>
2015-03-08 00:13:11 +1000

    Improved kernel build tool. (Closes #12)

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-KVER=3.18.8
+name=linux
+version=3.18.8
 
 
-if [ ! -f linux-$KVER.tar.xz ]; then
-  curl -q -L -O https://www.kernel.org/pub/linux/kernel/v3.x/linux-${KVER}.tar.xz
+if [ ! -f $name-$version ]; then
+  curl -q -L -O https://www.kernel.org/pub/$name/kernel/v${version%%.*}.x/$name-${version}.tar.xz
 fi
 
-if [ ! -d linux-$KVER ]; then
-  tar -xvf linux-${KVER}.tar.xz
-  cd linux-$KVER
+if [ ! -d $name-$version ]; then
+  tar -xvf $name-${version}.tar.xz
+  cd $name-$version
   for i in ../patches/*.patch; do
     patch -p1 < $i
   done
 else
-  cd linux-$KVER
+  cd $name-$version
 fi
 
 if [ ! -f .config ]; then
-  make defconfig
+  cp ../config-$version .config
 fi
 
 make bzImage
