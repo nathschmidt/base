ecbaf5f50e15a63b30984e713f868ddaeb76f20c
James Mills <prologic@shortcircuit.net.au>
2015-03-08 13:43:00 +1000

    Fix kernel sources from being re-downloaded each time. (Fixes #13)

--- kernel/build.sh
+++ kernel/build.sh
@@ -4,22 +4,16 @@ name=linux
 version=3.18.8
 
 
-if [ ! -f $name-$version ]; then
-  curl -q -L -O https://www.kernel.org/pub/$name/kernel/v${version%%.*}.x/$name-${version}.tar.xz
-fi
-
 if [ ! -d $name-$version ]; then
+  curl -q -L -O https://www.kernel.org/pub/$name/kernel/v${version%%.*}.x/$name-${version}.tar.xz
   tar -xvf $name-${version}.tar.xz
   cd $name-$version
   for i in ../patches/*.patch; do
     patch -p1 < $i
   done
+  cp ../config-$version .config
 else
   cd $name-$version
-fi
-
-if [ ! -f .config ]; then
-  cp ../config-$version .config
 fi
 
 make bzImage
