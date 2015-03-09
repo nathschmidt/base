60b52bc11fbaa01098dadbcfe2db955067921ddb
James Mills <prologic@shortcircuit.net.au>
2015-03-09 10:10:22 +1000

    Add a -b switch to test.sh for optional bridging

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 gui=0
+bridge=""
 
-while getopts g opt; do
+while getopts gb: opt; do
   case $opt in
   g)
-      gui=1
-      ;;
+    gui=1
+    ;;
+  b)
+    bridge=$OPTARG
   esac
 done
 
@@ -15,8 +18,14 @@ shift $((OPTIND - 1))
 echo "Booting..."
 echo
 
-if (( gui == 1 )); then
-  qemu-system-x86_64 -cdrom image.iso -net bridge,br=br0 -net nic
+if [ -n "$bridge" ]; then
+  network=-net bridge,br=br0 -net nic
 else
-  qemu-system-x86_64 -nographic -cdrom image.iso -net bridge,br=br0 -net nic
+  network=""
+fi
+
+if (( gui == 1 )); then
+  qemu-system-x86_64 -cdrom image.iso $network
+else
+  qemu-system-x86_64 -nographic -cdrom image.iso $network
 fi
