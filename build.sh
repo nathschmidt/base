7ef6b86605d2676db49296fce785cfc57327fb39
James Mills <prologic@shortcircuit.net.au>
2015-03-09 19:39:30 +1000

    Add support for building a rootfs tarball for a Docker image. (Closes #21)

--- build.sh
+++ build.sh
@@ -6,15 +6,22 @@ for pkg in packages/*; do
   (cd $pkg && fakeroot pkgmk -d)
 done
 
-rm -rf rootfs rootfs.cpio.gz
+rm -rf rootfs rootfs.tar.xz rootfs.cpio.gz
 
 mkdir rootfs
 
 for pkg in packages/*/*#*; do
-  tar -C rootfs -xvf $pkg
+  tar -C rootfs -xf $pkg
 done
 
 (cd rootfs && find . | cpio -H newc -o | gzip) > rootfs.cpio.gz
+
+tar --numeric-owner \
+    --exclude=/init \
+    --exclude=/etc/mtab \
+    --exclude=/root/.bash_history \
+    -P -cJf rootfs.tar.xz \
+    -C rootfs ./
 
 export ROOTFS=$(pwd)/rootfs.cpio.gz
 export OUTPUT=$(pwd)
