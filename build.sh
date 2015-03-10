45bee24f9c69ec10100c4d805633a100f88c67ce
James Mills <prologic@shortcircuit.net.au>
2015-03-10 23:42:22 +1000

    Improved build script

--- build.sh
+++ build.sh
@@ -1,34 +1,60 @@
 #!/bin/bash
 
-if [ "$UID" != "0" ]; then
-  echo "You need to be root to do this."
-  exit 1
-fi
 
-export PATH="$(pwd)/tools:$PATH"
+warn () {
+  echo "Warning:" "$@" >&2
+}
 
-for pkg in packages/*; do
-  (cd $pkg && fakeroot pkgmk -d)
+die () {
+  rc=$1
+  shift
+  echo "ERROR:" "$@" >&2
+  exit $rc
+}
+
+export TOP=$(pwd)
+export PATH="$TOP/tools:$PATH"
+export ROOTFS=$TOP/rootfs.cpio.gz
+export OUTPUT=$TOP
+
+packages=(busybox dropbear finit fs rc)
+
+echo "Building packages ..."
+for package in ${packages[@]}; do
+  (
+    echo " Building $package ..."
+    cd packages/$package
+    fakeroot pkgmk -d &> $TOP/$package.log || die 1 "Building $package failed! See: $package.log"
+  ) || die 1 "Error building packages!"
 done
 
-rm -rf rootfs rootfs.tar.xz rootfs.cpio.gz
 
+echo "Building rootfs ... (root required)"
+
+rm -rf rootfs.tar.xz rootfs.cpio.gz
+sudo rm -rf rootfs
 mkdir rootfs
 
-for pkg in packages/*/*#*; do
-  tar --numeric-owner -C rootfs -xf $pkg
+for package in ${packages[@]}; do
+  sudo tar --numeric-owner -C rootfs -xf packages/$package/$package#*.tar.gz
 done
 
-(cd rootfs && find . | cpio -H newc -o | gzip) > rootfs.cpio.gz
+(
+  cd rootfs
+  find . | sudo cpio -H newc -o | gzip
+) > rootfs.cpio.gz
 
-tar --numeric-owner \
+sudo tar --numeric-owner \
     --exclude=./init \
     --exclude=./etc/mtab \
     --exclude=./root/.bash_history \
     -P -cJf rootfs.tar.xz \
     -C rootfs ./
 
-export ROOTFS=$(pwd)/rootfs.cpio.gz
-export OUTPUT=$(pwd)
+echo "Building kernel and image ..."
+(
+  cd kernel
+  ./build.sh &> $TOP/kernel.log
+)
 
-(cd kernel && ./build.sh)
+echo "All done!"
